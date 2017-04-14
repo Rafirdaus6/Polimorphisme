ac8930511475eff945f8e5511de78e854a4f2db7
Rayyan Firdaus <rafirdaus006@gmail.com>
2017-04-14 17:30:55 +0700

    Add files via upload

---
+++ polymorphisme.java
@@ -0,0 +1,8 @@
+public class polymorphisme extends Player {
+		public static void main(String[] args) {
+			Player data = new Player() ;
+			data.contoh("Rayyan Firdaus");
+			data.contoh("660");
+			data.contoh("10.696");
+			}
+}
